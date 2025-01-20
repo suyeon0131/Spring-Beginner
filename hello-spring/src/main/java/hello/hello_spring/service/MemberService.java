ed493cccc1a29edba14952e5dd963baf4bdff9bf
suyeon0131 <suyeon0131@ajou.ac.kr>
2025-01-21 00:55:45 +0900

    Feat: 스프링 데이터 JPA

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,11 +5,13 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 // @Service // 스프링 컨테이너에 등록
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -24,6 +26,7 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
+
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
         return member.getId();
